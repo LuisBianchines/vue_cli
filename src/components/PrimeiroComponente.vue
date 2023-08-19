e08cc323cc35d2cf5d8864d9276c2636528e6897
LuisBianchine <luishdb@hotmail.com>
2023-08-19 18:41:54 -0300

    Mais sobre componentes vue.js

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        name: 'PirmeiroComponente',
+        data () {
+            return {
+                nome : "Luis",
+                profissao: "Programador"
+            }
+        }
+    }
+</script>
