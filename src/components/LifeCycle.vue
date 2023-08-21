fb20eb6d91673099a81d7bcf37e432b768700c13
LuisBianchine <luishdb@hotmail.com>
2023-08-21 11:40:25 -0300

    Fundamentos Métodos

--- src/components/LifeCycle.vue
+++ src/components/LifeCycle.vue
@@ -16,13 +16,22 @@
         created(){
             setTimeout(() => {
                 this.valor = '9000'    
-            }, 1000);           
+            }, 1000);   
+            
+            this.LifeCycle()
         },
         mounted(){
             setTimeout(() => {
                 this.valor = '2000'    
             }, 2000);
+
+            this.LifeCycle()
             
+        },
+        methods: {
+            LifeCycle(){
+                console.log('Deu certo');
+            }
         }
     }
 </script>
