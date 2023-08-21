3302489c2343ef9f0c835d8009df8691d18a0fbb
LuisBianchine <luishdb@hotmail.com>
2023-08-21 10:12:58 -0300

    Fundamentos de hierarquia de componentes

---
+++ src/components/info.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Estou trabalhando no momento</p>
+        <p>Utilizo as seguintes linguagens:</p>
+        <ul>
+            <li>Delphi</li>
+            <li>JavaScript</li>                
+        </ul>
+        <p>E os seguintes Frameworks</p>   
+        <ul>
+            <li>Vue.js</li>
+            <li>Horse</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'InfoPessoa'
+    }
+</script>
