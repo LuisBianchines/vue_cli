c97774096ea3115dea0b2f8fce6cd0c1d07bbf38
LuisBianchine <luishdb@hotmail.com>
2023-08-21 11:23:20 -0300

    Fundamentos Atributos Dinâmicos

--- src/components/info.vue
+++ src/components/info.vue
@@ -4,9 +4,10 @@
         <p v-else>Buscando novas oportunidades</p>
         <p v-if="4 > 6">Maior</p>
         <p v-else>menor</p>   
-
         <p v-show="mostrar_email">Enviar email para {{ email }}</p>
-        
+        <p>Para acessar meu portfólio <a v-bind:href="meu_link">Clique Aqui</a></p>
+        <Picture />   
+
         <p>Utilizo as seguintes linguagens:</p>
         <ul>
             <li>Delphi</li>
@@ -21,13 +22,18 @@
 </template>
 
 <script>
+    import Picture from './Picture.vue';
     export default {
         name : 'InfoPessoa',
+        components: {
+            Picture
+        },
         data(){
             return {
                 trabalhando: false,
                 email: 'luishdb@hotmail.com',
-                mostrar_email: false
+                mostrar_email: true,
+                meu_link: 'http://google.com'                
             }
         }
     }
