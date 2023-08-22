ce5cfbab45cfa74617b21c652ecb9b26a5175c0e
LuisBianchine <luishdb@hotmail.com>
2023-08-22 10:01:26 -0300

    Fundamentos Emit

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,15 +1,24 @@
 <template>
-    <img :src="avatar" :alt="descricao">
+  <img :src="avatar" :alt="descricao" />
+  <MudarImagem @mudar-imagem="TrocarImagem"/>
 </template>
 
 <script>
-    export default {
-        name: 'PictureTeste',
-        data(){
-            return{
-                avatar: "/img/avatar.png",
-                descricao: "Teste"
-            }
+import MudarImagem from './MudarImagem.vue';
+
+export default {
+    name: "PictureTeste",
+    data() {
+        return {
+            avatar: "/img/avatar.png",
+            descricao: "Teste",
+        };
+    },
+    methods: {
+        TrocarImagem() {
+            this.avatar = "/img/avatar2.png"
         }
-    } 
+    },
+    components: { MudarImagem }
+};
 </script>
